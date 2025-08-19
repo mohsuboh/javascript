b0659bf7d814e41678765499e99a1a9d158b6b63
mohamed suboh <moh.suboh2003@gmail.com>
2025-08-19 16:35:55 +0300

    hfu

--- personaltest.js
+++ personaltest.js
@@ -1,46 +1,42 @@
-function add()
-{
- const input=document.getElementById("userinput");
- const text =input.value;
- if (text === "") {
+function add() {
+    const input = document.getElementById("userinput");
+    let text = input.value;
+    if (text === "") {
         alert("Please enter some text!");
         return;
     }
-    const newdiv=document.createElement("div");
-    
-    newdiv.style.margin="5px";
-    newdiv.style.padding="5px";
-    newdiv.style.border="solid 2px";
+    const newdiv = document.createElement("div");
+
+    newdiv.style.margin = "5px";
+    newdiv.style.padding = "5px";
+    newdiv.style.border = "solid 2px";
     // Create span for text
-    const checkbox=document.createElement("input");
-    checkbox.type="checkbox";
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
 
     const span = document.createElement("span");
-    span.textContent = text;
-    span.style.paddingLeft="10px"
-    checkbox.onclick = function ()
-    {
-        if(checkbox.checked)
-            {
-                span.innerHTML = `<del>${text}</del>`;
-            }
-            else
-            {
-                span.innerHTML = text;
+    span.innerHTML = text;
+    span.style.paddingLeft = "10px"
 
-            }
+    checkbox.onclick = function () {
+       let newT = text
+        if (checkbox.checked) {
+           newT = `<del>${text}</del>`;
+        }
+        span.innerHTML = newT;
     }
 
 
 
     newdiv.appendChild(checkbox);
     newdiv.appendChild(span);
-    
-    
+
+
     document.getElementById("output").appendChild(newdiv);
 
-        input.value = "";
+    input.value = "";
 
 
 }
+
